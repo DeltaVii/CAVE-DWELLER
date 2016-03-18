d4b0f7462690ed68b073bed8ead350709a9e983d
Kelly Norris <samtimmus@gmail.com>
2016-03-18 13:05:26 -0700

    Progress
    Got the fonts to work, and started on the menu cursor

--- CAVE_DWELLER/MAIN.py
+++ CAVE_DWELLER/MAIN.py
@@ -28,6 +28,7 @@ size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Window Title goes here")
 
+#importing classes
 rooms = Rooms()
 menu = Menu()
 
@@ -38,7 +39,8 @@ done = False
 clock = pygame.time.Clock()
 
 #Other variables for looping:
-
+font = pygame.font.Font('AdobeArabic-Regular.otf', 50)
+cursor_position = 'menu'
 #-----Main Loop-----#
 while not done:
     #Event loop, looking for inputs
@@ -48,22 +50,42 @@ while not done:
             done = True
         elif event.type == pygame.KEYDOWN:
             print("User pressed a key")
+            if cursor_position == 'menu':
+                if event.key == pygame.K_DOWN:
+                    cursor_position = 'move'
+                if event.key == pygame.K_RIGHT:
+                    cursor_position = 'interact'
+            if cursor_position == 'move':
+                if event.key == pygame.K_UP:
+                    cursor_position = 'menu'
+            if cursor_position == 'interact':
+                if event.key == pygame.K_LEFT:
+                    cursor_position = 'menu'
         elif event.type == pygame.KEYUP:
             print("User let go of a key")
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print("User pressed a mouse button")
     #Game logic goes here
-
+    
     #clear screen
     screen.fill(BLACK)
 
     #Draws go here
     
+    
     menu.drawMenuBox(screen, WHITE)
+    menu.drawMenuText(screen, font, WHITE)
+    #if cursor_position == 'menu':
+        #menu.drawMenuCursor(screen, WHITE, 130,130,140,375,395,385)
+    #if cursor_position == 'move':
+        #menu.drawMenuCursor(screen, WHITE, 130,130,140,425,445,435)
+    #if cursor_position == 'interact':
+        #menu.drawMenuCursor(screen, WHITE, 400,400,410,375,395,385)
+    menu.drawMenuCursorSimple(screen, WHITE)
     if basegame.current_room == 0:
         rooms.drawHouse(screen, WHITE)
-        #menu.drawRoom0Text(
-
+        menu.drawRoom0Text(screen, font, WHITE)
+    
     #Make sure draws go on screen
     pygame.display.flip()
 
